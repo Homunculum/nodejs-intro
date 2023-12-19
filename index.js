5c69ea54f5c3fff20e2b0581f89474f8e08d73f8
Abdulkadir Elsıkma <a.kadir.e.91@gmail.com>
2023-12-19 22:03:12 +0300

    v1

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-function topla (sayi1, sayi2){
-    return sayi1 + sayi2
+class Customer{
+    constructor(id,customerNumber){
+        this.id = id;
+        this.customerNumber = customerNumber;
+    }
+} 
+
+let customer = new Customer(1, "12345");
+//prototyping
+customer.name = "Murat Kurtboğan"
+console.log(customer.name)
+
+customer.bisey = "bisey"
+console.log(Customer.bisey)
+
+console.log(Customer.customerNumber)
+
+class IndividualCustomer extends Customer{
+    constructor(firstName,id,customerNumber){
+        super(id,customerNumber)
+        this.firstName = firstName
+    }
 }
 
-console.log(topla(5,10));
-console.log(topla());
-console.log(topla(1));
-console.log(topla(5,"1"));
-console.log(topla(5,"1",5,10));
+class CorporCustomer extends Customer{
+    constructor(companyName,id,customerNumber){
+        super(id,customerNumber)
+        this.companyName = companyName
+    }
+}
+
+
+let products = [
+    {id:1, name: "Acer Laptop", unitPrice: 15000}, 
+    {id:2, name: "Asus Laptop", unitPrice: 16000}, 
+    {id:3, name: "Hp Laptop", unitPrice:13000}, 
+    {id:4, name: "Dell Laptop", unitPrice: 12000}, 
+    {id:5, name: "Casper Laptop", unitPrice: 17000}
+]
+
+console.log("<ul>")
+products.map(product => console.log(`<li>${product.name}</li>`))
+console.log("</ul>")
