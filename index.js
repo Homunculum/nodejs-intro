883b9baa87837a884047bc5f49854381feeb6057
Abdulkadir Elsıkma <a.kadir.e.91@gmail.com>
2023-12-19 22:01:44 +0300

    homework

---
+++ index.js
@@ -0,0 +1,9 @@
+function topla (sayi1, sayi2){
+    return sayi1 + sayi2
+}
+
+console.log(topla(5,10));
+console.log(topla());
+console.log(topla(1));
+console.log(topla(5,"1"));
+console.log(topla(5,"1",5,10));
